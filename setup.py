c9be091e14b3fb61c4d3d1db16728b14c3898e3b
hariprasadkulal650@gmail.com <142109689+hariiiiiiiiiiiiii@users.noreply.github.com>
2025-06-11 12:36:51 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='generative AI Project',
+    version='0.0.0',
+    author='Hariprasad Kulal',
+    author_email='hariprasadkulal650@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
